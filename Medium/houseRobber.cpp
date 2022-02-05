74cece82837a711e7cd11ed29bf14233f2e94a39
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-02-05 23:56:18 +0530

    Create houseRobber.cpp

---
+++ Medium/houseRobber.cpp
@@ -0,0 +1,32 @@
+//Recursion
+
+class Solution {
+public:
+    int rec(vector<int>& nums, int idx){
+        if (idx>=nums.size())
+            return 0;
+        return max(nums[idx]+rec(nums, idx+2), rec(nums, idx+1));
+    }
+    
+    int rob(vector<int>& nums) {
+        return rec(nums,0);
+    }
+};
+
+//Memoized
+
+class Solution {
+public:
+    int rec(vector<int>& nums, int idx, vector<int>& dp){
+        if (idx>=nums.size())
+            return 0;
+        if (dp[idx]!=-1)
+            return dp[idx];
+        return dp[idx]= max(nums[idx]+rec(nums, idx+2, dp), rec(nums, idx+1, dp));
+    }
+    
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,-1);
+        return rec(nums,0,dp);
+    }
+};
