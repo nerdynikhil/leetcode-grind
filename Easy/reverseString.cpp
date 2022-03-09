7dc95a6c2f0b34e8b224d746a11f7f4d35ee6dc3
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-03-10 00:42:55 +0530

    add recursive approach

--- Easy/reverseString.cpp
+++ Easy/reverseString.cpp
@@ -10,3 +10,18 @@ public:
         }
     }
 };
+
+//Using recursion
+
+class Solution {
+public:
+    void helper(vector<char>& s, int index){
+        if (index>=s.size()/2)
+            return;
+        swap(s[index], s[s.size()-index-1]);
+        helper(s, index+1);
+    }
+    void reverseString(vector<char>& s) {
+        helper(s,0);
+    }
+};
