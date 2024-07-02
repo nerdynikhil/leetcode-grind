ec35c6d03b7bbfca8db6043f335fd374e8bcfefc
Nikhil Barik <nklmarch17@gmail.com>
2024-07-03 05:28:21 +0530

    Time: 0 ms (100%), Space: 40.1 MB (49.84%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,21 @@
 class Solution {
-    private int findFib(int n){
-        if (n==0 || n==1)
+    private int findFib(int n, HashMap<Integer, Integer> mp) {
+        if (n == 0 || n == 1)
             return n;
-        
-        int a = findFib(n-1);
-        int b = findFib(n-2);
 
-        return a+b;
+        int currentKey = n;
+
+        if (mp.containsKey(currentKey))
+            return mp.get(currentKey);
+
+        int a = findFib(n - 1, mp);
+        int b = findFib(n - 2, mp);
+
+        mp.put(currentKey, a + b);
+        return mp.get(currentKey);
     }
+
     public int fib(int n) {
-        return findFib(n);
+        return findFib(n, new HashMap<Integer, Integer>());
     }
 }
