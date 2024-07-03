d182173b90c62a5fb75615e3bba7499c9b356695
Nikhil Barik <nklmarch17@gmail.com>
2024-07-03 06:38:27 +0530

    Time: 0 ms (100%), Space: 40.8 MB (6.66%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,27 +1,22 @@
 class Solution {
-    int findTrib(int n, HashMap<Integer, Integer> memo) {
+    private int findTribonacci(int n, HashMap<Integer, Integer> cache) {
         if (n == 0)
             return 0;
-
         if (n == 1 || n == 2)
             return 1;
 
         int currentKey = n;
 
-        if (memo.containsKey(currentKey))
-            return memo.get(currentKey);
+        if (cache.containsKey(currentKey)) {
+            return cache.get(currentKey);
+        }
 
-        int a = findTrib(n - 1, memo);
-        int b = findTrib(n - 2, memo);
-        int c = findTrib(n - 3, memo);
-
-        memo.put(currentKey, a + b + c);
-
-        return memo.get(currentKey);
+        cache.put(currentKey,
+                findTribonacci(n - 1, cache) + findTribonacci(n - 2, cache) + findTribonacci(n - 3, cache));
+        return cache.get(currentKey);
     }
 
     public int tribonacci(int n) {
-        HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
-        return findTrib(n, memo);
+        return findTribonacci(n, new HashMap<Integer, Integer>());
     }
 }
