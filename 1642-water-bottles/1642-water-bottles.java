f15fca0d0cb9a7312f0410603bf6f0b527dcbb7f
Nikhil Barik <nklmarch17@gmail.com>
2024-07-08 01:19:31 +0530

    Time: 0 ms (100%), Space: 40.1 MB (54.76%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,18 +1,12 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-
-        int totalBottles = numBottles; // 9
-
-        while (numBottles >= numExchange){ // 3 == 3
-
-            int r = numBottles % numExchange;
-            numBottles = numBottles / numExchange; // 3/3
-          
-            totalBottles = totalBottles + numBottles; // 9+3 12
-
-            numBottles =  numBottles + r; // 3 + 3%3 = 3 + 0= 3
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            int remainder = numBottles % numExchange;
+            numBottles = numBottles / numExchange;
+            ans += numBottles;
+            numBottles += remainder;
         }
-
-        return totalBottles;
+        return ans;
     }
 }
