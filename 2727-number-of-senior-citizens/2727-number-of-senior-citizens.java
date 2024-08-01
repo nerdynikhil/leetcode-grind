d1b35b92c70b157bf305c4151267e98bc804b586
Nikhil Barik <nklmarch17@gmail.com>
2024-08-01 23:34:11 +0530

    Time: 1 ms (82.64%), Space: 42.5 MB (57.57%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+
+        for (String detail : details) {
+            String ageStr = detail.substring(11, 13);
+            int age = Integer.parseInt(ageStr);
+
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
