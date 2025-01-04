93da86f61889b8b9d9ced7d1a040f782a672953e
Nikhil Barik <nklmarch17@gmail.com>
2025-01-04 22:47:30 +0530

    Time: 1 ms (83.85%), Space: 41.9 MB (52.07%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int largestLength = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < largestLength; i++) {
+            if (i < word1.length()) {
+                ans.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                ans.append(word2.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
